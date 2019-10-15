12c9e6e0e5a5226878e8b663abf3002ce31131f8
Manishajagadale <56343366+Manishajagadale@users.noreply.github.com>
2019-10-15 18:46:05 +0530

    15/10/2019

---
+++ contsant.js
@@ -0,0 +1,8 @@
+
+window.onload = function () {
+    const PI = 3.14;
+    PI = 10;
+    console.log(PI); function calcArea(r) {
+        console.log
+            ("The
+        area is : " + PI * r * r) } calcArea(5); } /script> </head > </html >
